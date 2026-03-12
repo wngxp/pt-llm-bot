4bfd4ff93eea023ad04531a2a8dd9f3d9c329025
wngxp <x253wang@uwaterloo.ca>
2026-03-12 12:12:48 +0800

    sanity

--- restart.sh
+++ restart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-cd ~/pt-llm-bot 2>/dev/null || cd /Users/wxp/dev/pt-llm-bot
+cd ~/pt-llm-bot
 
+git stash
 git pull --rebase
+git stash pop || true
 
 export https_proxy=http://127.0.0.1:7890
 export http_proxy=http://127.0.0.1:7890
